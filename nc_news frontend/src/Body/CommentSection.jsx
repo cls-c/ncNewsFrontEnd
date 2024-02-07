d37291c84b1aa4cc45bebc0257bdbf409edb7bc7
cls-cheung <sabrinacheung1029@gmail.com>
2024-02-07 09:55:49 +0000

    added loading component in comments

--- nc_news frontend/src/Body/CommentSection.jsx
+++ nc_news frontend/src/Body/CommentSection.jsx
@@ -3,32 +3,42 @@ import CommentCard from "../Components/Comment";
 import { useContext, useEffect, useState } from "react";
 import { fetchCommentByArticleId } from "../util/api";
 import { userContext } from "../contexts/userContext";
+import LoadingSpinner from "../Components/LoadingSpinner";
 
 export default function CommentSection() {
   const [commentArr, setCommentArr] = useState([]);
-  const { selectedArticleId } = useContext(userContext);
+  const { selectedArticleId, isLoading, setIsLoading } =
+    useContext(userContext);
 
   function fetchAndUpdateComments() {
-    fetchCommentByArticleId(selectedArticleId).then(
-      (data) => {
-        setCommentArr(data);
-      }
-    );
+    setIsLoading(true);
+    fetchCommentByArticleId(selectedArticleId).then((data) => {
+      setCommentArr(data);
+      setIsLoading(false);
+    });
   }
 
   useEffect(() => {
     fetchAndUpdateComments();
   }, []);
-  return (
-    <>
-      <Container>
-        <Row className="justify-content-centre">
-          Comment Section
-          {commentArr.map((comment,index)=>{
-            return <CommentCard comment={comment} key={`comment ${index}`}/>
-          })}
-        </Row>
-      </Container>
-    </>
-  );
+  let returnBody;
+  {
+    isLoading
+      ? (returnBody = <LoadingSpinner />)
+      : (returnBody = (
+          <>
+            <Container>
+              <Row className="justify-content-centre">
+                Comment Section
+                {commentArr.map((comment, index) => {
+                  return (
+                    <CommentCard comment={comment} key={`comment ${index}`} />
+                  );
+                })}
+              </Row>
+            </Container>
+          </>
+        ));
+  }
+  return returnBody;
 }
